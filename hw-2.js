fd052f028aad5dc92006a0bfbc4f7ae8787cb903
Полина <zeganovapolina@gmail.com>
2024-11-28 21:06:31 +0400

    Добавила js

---
+++ hw-2.js
@@ -0,0 +1,57 @@
+//Задание1//
+let cat = 10;
+alert (cat);
+cat = 20;
+alert (cat);
+
+//Задание2//
+const phone = 2007;
+alert (phone);
+
+//Задание3//
+const tip = 'Брендан';
+alert (tip);
+
+//Задание4//
+let star = 10;
+let sun = 2;
+let sum = star + sun;
+let dif = star - sun;
+let mult = star * sun;
+let div = star / sun;
+alert(`Сумма: ${sum}, Разность: ${dif}, Умножение: ${mult}, Деление: ${div}`);
+
+//Задание5//
+let result = 2 ** 5;
+alert (result);
+
+//Задание6//
+let a = 9;
+let b = 2;
+let remainder = a % b;
+alert (remainder);
+
+//Задание7//
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num ++;
+num --;
+alert(num);
+
+//Задание8//
+let age = prompt("Сколько вам лет?");
+alert(age);
+
+//Задание9//
+const user = {
+    name: 'Mark',
+    age: 18,
+    isAdmin: true
+}
+
+//Задание10//
+let name = prompt("Как Вас зовут?");
+alert(`Привет, ${name}!`);
